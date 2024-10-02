361bcdcbe26dc15f9f14b83830f508ad8493f442
Sumiran Jaiswal <sumi0309@umd.edu>
2024-10-02 12:55:56 -0400

    Project Setup

---
+++ Up-To-Date (UTD)/Up-To-Date (UTD)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Up_To_Date__UTD_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
